9d24c5eb9642e632b3912d3c75169766c628c7b3
Chaitanya-Saswade <chaitanyasaswade7535@gmail.com>
2026-08-02 23:07:48 +0530

    AlMoodMovieFinder

--- AIMoodMovieFinder/script.js
+++ AIMoodMovieFinder/script.js
@@ -1,10 +1,10 @@
-const buttons = document.QuerySelectorAll("button")
+const buttons = document.querySelectorAll("button")
     //Here buttons are Data of Array i.e Store Multiple Buttons
 
-const moviesContainer = document.getElementbyId("movies-container")
+const moviesContainer = document.getElementById("movies-container")
 
 buttons.forEach(btn => {
-    btn.addEventlistener('click', () => {
+    btn.addEventListener('click', () => {
 
         const mood = btn.dataset.mood;
 
@@ -12,4 +12,41 @@ buttons.forEach(btn => {
     });
 });
 
-//To Get the movies on the basis of Mood Value
+// Task-2 To Get the movies on the basis of Mood Value
+
+async function fetchMovie(movieName) {
+
+    moviesContainer.innerHTML = "<h2>Loading.....</h2>";
+
+    const url = `http://www.omdbapi.com/?i=tt3896198&apikey=fee02d16&s=${movieName}`;
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    displayMovie(data.Search)
+
+}
+
+
+function displayMovie(movies) {
+
+    moviesContainer.innerHTML = "";
+
+    movies.forEach(movie => {
+
+        const card = document.createElement("div")
+
+        card.classList.add("movie-card")
+
+        card.innerHTML = ` 
+        <img src = "${movie.Poster}" >
+
+        <div class = "movie-info">
+        <h3> ${movie.Title} </h3> 
+        <p> ${movie.Year} </p> 
+        </div>
+    `;
+
+        moviesContainer.appendChild(card)
+    })
+}
