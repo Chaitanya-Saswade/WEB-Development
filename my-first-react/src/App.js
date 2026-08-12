1b7c122d16eacc02933d9ed28a7a8bfb5304c2b7
Chaitanya-Saswade <chaitanyasaswade7535@gmail.com>
2026-08-13 00:08:37 +0530

    React

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// Div Tag acts like a Fragment
+
+
+return(
+
+        <div>
+        <h1>Hello Guys, How are You Doing...?</h1>
+        <button>Log IN</button>
+
+        {/* Every Tag in React should be Closed. */}
+        <label>UserName: </label>
+        <input  />
+        </div>
+)
 }
 
 export default App;
