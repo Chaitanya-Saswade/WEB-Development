1a79cfba67f1d9696b12bfa1e9ff2cc8f5e3ae04
Chaitanya-Saswade <chaitanyasaswade7535@gmail.com>
2026-08-13 01:49:05 +0530

    Add TO Cart Page

--- my-first-react/src/App.js
+++ my-first-react/src/App.js
@@ -1,20 +1,31 @@
-import React from "react"
+import React from "react";
+import Btn from "./Button";
 
 function App() {
 
 // Div Tag acts like a Fragment
 
-
 return(
 
         <div>
         <h1>Hello Guys, How are You Doing...?</h1>
-        <button>Log IN</button>
+        <hr></hr>
 
+        
         {/* Every Tag in React should be Closed. */}
         <label>UserName: </label>
         <input  />
+        <br></br>
+
+        <button>Log IN</button>
+
+        <Btn />
+        <Btn />
+        <Btn />
         </div>
+
+
+
 )
 }
 
